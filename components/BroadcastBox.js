8a3e49361f430b887cd1cd197eb1b64f89eeac32
Neil Sarkar <neil.r.sarkar@gmail.com>
2017-01-08 17:01:11 +0000

    Layout broadcast box

--- components/BroadcastBox.js
+++ components/BroadcastBox.js
@@ -3,14 +3,42 @@
 import React, {Component} from 'react';
 
 import {
+  StyleSheet,
   Text,
-  View
+  TextInput,
+  TouchableOpacity,
+  View,
 } from 'react-native';
+
+const base = require('../styles/base');
 
 export default class BroadcastBox extends Component {
   render() { return (
-    <View>
-      <Text>Broadcast Box</Text>
+    <View style={[base.padded, styles.container]}>
+      <TextInput style={[styles.input]} placeholder="Sup?"/>
+      <TouchableOpacity onPress={() => alert('Nope.')}>
+        <Text style={[base.padded, styles.button]}>send</Text>
+      </TouchableOpacity>
     </View>
   )}
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flexDirection: 'row',
+    alignItems: 'center',
+  },
+  input: {
+    backgroundColor: 'whitesmoke',
+    flex: 1,
+    fontSize: 13,
+    padding: 4,
+    height: 26,
+  },
+  button: {
+    color: 'lawngreen',
+    paddingTop: 0,
+    paddingRight: 0,
+    paddingBottom: 0,
+  }
+})
