b1153c0627c03d5847455fc52ca6d77a724c0137
Neil Sarkar <neil.r.sarkar@gmail.com>
2017-01-08 16:48:13 +0000

    Layout friends with images

--- components/Friend.js
+++ components/Friend.js
@@ -20,8 +20,8 @@ export default class Friend extends Component {
 
   render() { return (
     <View style={[styles.box, base.padded]}>
-      <Image />
-      <Text style={{flex: 1}}>{this.props.friend.name}</Text>
+      <Image style={{width: 40, height: 40}} source={{uri: 'https://placehold.it/80x80.png'}}/>
+      <Text style={[base.padded, styles.main]}>{this.props.friend.name}</Text>
       <TouchableOpacity onPress={this.toggle.bind(this)} accessible={true} accessibilityLabel={`Select ${this.props.friend.name}`}>
         { this.state.selected ?
           <Text>Yep</Text>
@@ -41,6 +41,11 @@ const styles = StyleSheet.create({
   box: {
     backgroundColor: 'grey',
     flexDirection: 'row',
+    alignItems: 'center',
   },
-
+  main: {
+    paddingTop: 0,
+    paddingBottom: 0,
+    flex: 1,
+  },
 });
