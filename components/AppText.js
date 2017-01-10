239b9d542153bf36499a023e4ed890adf90a88e2
Neil Sarkar <neil.r.sarkar@gmail.com>
2017-01-10 01:57:02 +0000

    Fix AppText to allow overwriting and passthru props

--- components/AppText.js
+++ components/AppText.js
@@ -8,11 +8,21 @@ import {
 } from 'react-native';
 
 export default class AppText extends Component {
+  constructor(props) {
+    super(props)
+    this.style = [{fontFamily: 'Poppins'}];
+    if( props.style ) {
+      if( Array.isArray(props.style) ) {
+        this.style = this.style.concat(props.style)
+      } else {
+        this.style.push(props.style)
+      }
+    }
+  }
+
   render() { return (
-    <Text {...this.props}>
-      <Text style={{fontFamily: 'Poppins'}}>
-        {this.props.children}
-      </Text>
+    <Text {...this.props} style={this.style}>
+      {this.props.children}
     </Text>
   )}
 }
