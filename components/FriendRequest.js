e75bbe2f362edf5648dd4daea60960dab3d07448
Neil Sarkar <neil.r.sarkar@gmail.com>
2017-01-15 13:53:21 +0000

    Rough draft of friend requests

--- components/FriendRequest.js
+++ components/FriendRequest.js
@@ -3,14 +3,51 @@
 import React from 'react';
 import Component from './Component';
 import Text from './Text';
+import base from '../styles/base';
 import {
+  Image,
+  StyleSheet,
   View,
 } from 'react-native';
 
 export default class FriendRequest extends Component {
   render() { return (
-    <View>
-      <Text>Friend Request</Text>
+    <View style={{flexDirection: 'row'}}>
+      <Image style={[styles.image]} source={{uri: this.props.friend.avatar_url}}/>
+      <View style={styles.right}>
+        <Text>{this.props.friend.name}</Text>
+        <View style={{flexDirection: 'row'}}>
+          <Text style={[styles.button, styles.confirm]}>confirm</Text>
+          <Text style={[styles.button, styles.nah]}>nah</Text>
+        </View>
+      </View>
     </View>
   )}
 }
+
+const styles = StyleSheet.create({
+  image: {
+    width: 40,
+    height: 40,
+  },
+
+  right: {
+    flex: 1,
+  },
+
+  button: {
+    paddingLeft: 20,
+    paddingRight: 20,
+    paddingTop: 5,
+    paddingBottom: 5,
+  },
+
+  confirm: {
+    backgroundColor: 'mediumaquamarine',
+    color: 'white',
+  },
+
+  nah: {
+    backgroundColor: base.colors.lightgrey
+  },
+})
