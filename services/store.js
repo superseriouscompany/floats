b3f78d3a94f6a03d49586f27ce489f78e30e9ebd
Neil Sarkar <neil.r.sarkar@gmail.com>
2017-01-15 11:11:18 +0000

    Add a bunch of states to the store

--- services/store.js
+++ services/store.js
@@ -8,6 +8,23 @@ function store(state, action) {
     case 'login':
       state.accessToken = action.accessToken;
       return state;
+    case 'loadNearbyFriends':
+      state.friends = action.friends;
+      return state;
+    case 'removeInvitee':
+      state.invitees = state.invitees.filter(function(i) {
+        return i.id != action.friend.id
+      })
+      return state;
+    case 'addInvitee':
+      state.invitees = state.invitees.concat([action.friend]);
+      return state;
+    case 'addAllInvitees':
+      state.invitees = [].concat(state.friends);
+      return state;
+    case 'removeAllInvitees':
+      state.invitees = [];
+      return state;
     case '@@redux/INIT':
       return state;
     default:
