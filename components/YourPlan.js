4253af99f96836a6f9b825c6f7823a70c7f07032
Neil Sarkar <neil.r.sarkar@gmail.com>
2017-01-15 02:24:08 +0000

    Allow deleting floats

--- components/YourPlan.js
+++ components/YourPlan.js
@@ -6,10 +6,12 @@ import Plan     from './Plan';
 import Attendee from './Attendee';
 import Component from './Component';
 import base from '../styles/base';
+import api from '../services/api';
 import {
   Image,
   TouchableOpacity,
   View,
+  AsyncStorage,
 } from 'react-native';
 
 export default class YourPlan extends Component {
@@ -22,14 +24,25 @@ export default class YourPlan extends Component {
     if( !plan ) { return null; }
     return (
     <View style={[{flexDirection: 'row', paddingBottom: 18, backgroundColor: base.colors.offwhite, borderBottomWidth: 1, borderBottomColor: base.colors.lightgrey}, base.padFullHorizontal, base.padMainItem]}>
-      <Image source={{url: this.props.plan.user.avatar_url}} style={base.photoCircle} />
+      <Image source={{url: plan.user.avatar_url}} style={base.photoCircle} />
       <View style={{flex: 1}}>
         <Plan plan={plan} attendees={plan.attendees}/>
         <Text style={[base.timestamp, {color: base.colors.mediumgrey}]}>text them to coordinate</Text>
-        <TouchableOpacity onPress={() => alert('Nope.')}>
-          <Text style={[base.timestamp, {textDecorationLine: 'underline', color: base.colors.mediumgrey}]}>or delete your plan</Text>
+        <TouchableOpacity onPress={this.deletePlan.bind(this)}>
+          <Text style={[base.timestamp, {textDecorationLine: 'underline', color: base.colors.mediumgrey}]}>or delete your float</Text>
         </TouchableOpacity>
       </View>
     </View>
   )}
+
+  deletePlan() {
+    AsyncStorage.getItem('@floats:accessToken').then((accessToken) => {
+      return api.floats.destroy(accessToken, this.props.plan.id).then(function() {
+        alert("Deleted");
+      });
+    }).catch(function(err) {
+      alert(err);
+      console.error(err);
+    })
+  }
 }
