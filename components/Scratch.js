3d3962504735137affec7a9fd5cdc86fd6a08d2a
Neil Sarkar <neil.r.sarkar@gmail.com>
2017-01-26 22:05:27 +0000

    Checkpoint: use container component

--- components/Scratch.js
+++ components/Scratch.js
@@ -10,6 +10,11 @@ import {
 import {createStore} from 'redux';
 import { Provider, connect } from 'react-redux';
 
+// Goals:
+//
+// * trigger api call when root component is not being displayed
+
+
 function reducer(state = {}, action) {
   switch(action.type) {
     case 'items:load':
@@ -38,25 +43,23 @@ function reducer(state = {}, action) {
 
 const store = createStore(reducer);
 
-export default class Scratch extends Component {
+class Scratch extends Component {
   render() { return (
     <Provider store={store}>
-      <Poop />
+      <Foo />
     </Provider>
   )}
 }
 
-class ScratchDumb extends Component {
+export default Scratch;
+
+class Foo extends Component {
   constructor(props) {
-    super(props)
-    this.state = { items: []};
+    super(props);
+    this.load();
   }
 
-  componentDidMount() {
-    this.reload();
-  }
-
-  reload() {
+  load() {
     const count = Math.floor(Math.random() * 10);
     let items = [];
     for( var i = 0; i < count; i++ ) {
@@ -65,7 +68,7 @@ class ScratchDumb extends Component {
 
     this.props.dispatch({type: 'items:load'});
     setTimeout( ()=> {
-      if( Math.random() > 0.5 ) {
+      if( Math.random() > 0 ) {
         this.props.dispatch({type: 'items:load:yes', items: items });
       } else {
         this.props.dispatch({type: 'items:load:no', error: new Error('Something went wrong') });
@@ -73,32 +76,12 @@ class ScratchDumb extends Component {
     }, 500);
   }
 
-  render() { return (
-    <View style={{flex: 1, alignItems: 'center', paddingTop: 20}}>
-      <TouchableOpacity onPress={this.reload.bind(this)}>
-        <Text>Reload</Text>
-      </TouchableOpacity>
-      <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
-        { this.props.loading ?
-          <Text>Loading...</Text>
-        : null }
-
-        { this.props.error ?
-          <Text style={{color: 'indianred'}}>{this.state.error}</Text>
-        : this.props.items ?
-          <View>
-            {this.props.items.map( (x, key) => (
-              <Text key={key}>{x}</Text>
-            ))}
-          </View>
-        : null
-        }
-
-        <Text>{ this.props.cool }</Text>
-      </View>
-    </View>
+  render() { console.log('re-rendered with', this.props); return (
+    <FooView {...this.props} load={this.load.bind(this)} />
   )}
 }
+
+Foo = connect(mapStateToProps)(Foo);
 
 function mapStateToProps(state) {
   return {
@@ -108,4 +91,48 @@ function mapStateToProps(state) {
   }
 }
 
-const Poop = connect(mapStateToProps)(ScratchDumb)
+class FooView extends Component {
+  constructor(props) {
+    super(props)
+
+    console.log("loaded with props", props);
+  }
+
+  render() { return (
+    <View style={{flex: 1, alignItems: 'center', paddingTop: 20}}>
+      <ReloadButton load={this.props.load}/>
+      <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
+        { this.props.loading ?
+          <Text>Loading...</Text>
+        : null }
+
+        { this.props.error ?
+          <Text style={{color: 'indianred'}}>{this.props.error.toString()}</Text>
+        : this.props.items ?
+          <View>
+            {this.props.items.map( (x, key) => (
+              <Text key={key}>{x}</Text>
+            ))}
+          </View>
+        : null
+        }
+      </View>
+    </View>
+  )}
+}
+
+FooView.propTypes = {
+  load: React.PropTypes.func.isRequired,
+}
+
+class ReloadButton extends Component {
+  render() { return (
+    <TouchableOpacity onPress={this.props.load}>
+      <Text>Reload</Text>
+    </TouchableOpacity>
+  )}
+}
+
+FooView.propTypes = {
+  load: React.PropTypes.func.isRequired,
+}
