ba2e5b85e83b78721a5e72e8ca6ec4e897ba0bec
Neil Sarkar <neil.r.sarkar@gmail.com>
2017-01-26 15:53:55 +0000

    Add loading and error states

--- components/Scratch.js
+++ components/Scratch.js
@@ -12,7 +12,7 @@ export default class Scratch extends Component {
   constructor(props) {
     super(props)
 
-    this.state = { things: []};
+    this.state = { items: []};
   }
 
   componentDidMount() {
@@ -21,13 +21,31 @@ export default class Scratch extends Component {
 
   reload() {
     const count = Math.floor(Math.random() * 10);
-    let things = [];
+    let items = [];
     for( var i = 0; i < count; i++ ) {
-      things.push(`Thing ${Math.floor(Math.random() * 100)}`)
+      items.push(`Thing ${Math.floor(Math.random() * 100)}`)
     }
+
     this.setState({
-      things: things,
+      loading: true,
+      error: null,
+      items: [],
     })
+
+    setTimeout( ()=> {
+      if( Math.random() > 0.5 ) {
+        this.setState({
+          items: items,
+          error: null,
+          loading: false,
+        })
+      } else {
+        this.setState({
+          error: 'Something went wrong...',
+          loading: false,
+        })
+      }
+    }, 500);
   }
 
   render() { return (
@@ -36,9 +54,19 @@ export default class Scratch extends Component {
         <Text>Reload</Text>
       </TouchableOpacity>
       <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
-        {this.state.things.map( (x, key) => (
-          <Text key={key}>{x}</Text>
-        ))}
+        { this.state.loading ?
+          <Text>Loading...</Text>
+        : null }
+
+        { this.state.error ?
+          <Text style={{color: 'indianred'}}>{this.state.error}</Text>
+        :
+          <View>
+            {this.state.items.map( (x, key) => (
+              <Text key={key}>{x}</Text>
+            ))}
+          </View>
+        }
       </View>
     </View>
   )}
