920daaec2c0e630ef73a34530613a2e1f37e40dc
Neil Sarkar <neil.r.sarkar@gmail.com>
2017-01-27 14:24:57 +0000

    Move all the behavior to the action

--- app/actions/nearbyFriends.js
+++ app/actions/nearbyFriends.js
@@ -1,3 +1,40 @@
+import FCM from 'react-native-fcm';
+import api from '../services/api';
+
 export function fetchNearbyFriends(dispatch) {
   dispatch({type: 'nearbyFriends:load'});
+  FCM.requestPermissions();
+  // FIXME: retry as long as it's not set
+  FCM.getFCMToken().then( (token) => {
+    if( !token ) { return console.warn("No firebase token available."); }
+    api.sessions.updateFirebaseToken(token);
+  });
+  FCM.on('refreshToken', (token) => {
+    if( !token ) { return console.warn("No firebase token on refresh."); }
+    api.sessions.updateFirebaseToken(token);
+  })
+
+  navigator.geolocation.getCurrentPosition(
+    (position) => {
+      var initialPosition = JSON.stringify(position);
+      api.pins.create({
+        lat: position.coords.latitude,
+        lng: position.coords.longitude,
+      }).then(function() {
+        return api.friends.nearby();
+      }).then((friends) => {
+        friends = friends.map(function(f) {
+          f.selected = true;
+          return f;
+        })
+        dispatch({type: 'nearbyFriends:load:yes', friends: friends});
+      }).catch(function(err) {
+        dispatch({type: 'nearbyFriends:load:no', error: err});
+      })
+    },
+    (error) => {
+      dispatch({type: 'nearbyFriends:load:no', error: error});
+    },
+    {enableHighAccuracy: true, timeout: 20000, maximumAge: 1000}
+  );
 }
--- app/containers/CreateFloatCtrl.js
+++ app/containers/CreateFloatCtrl.js
@@ -1,11 +1,9 @@
 'use strict';
 
 import React, { Component } from 'react';
-import FCM from 'react-native-fcm';
 import { connect } from 'react-redux';
 import CreateFloatScene from '../components/CreateFloatScene';
 import { fetchNearbyFriends } from '../actions/nearbyFriends';
-import api from '../services/api';
 import {
   AsyncStorage
 } from 'react-native';
@@ -19,45 +17,6 @@ class CreateFloatCtrl extends Component {
   componentDidMount() {
     this.setState({loading: true});
     this.props.dispatch(fetchNearbyFriends);
-
-    AsyncStorage.getItem('@floats:accessToken').then((accessToken) => {
-      FCM.requestPermissions();
-      // FIXME: retry as long as it's not set
-      FCM.getFCMToken().then( (token) => {
-        if( !token ) { return console.warn("No firebase token available."); }
-        api.sessions.updateFirebaseToken(token);
-      });
-      FCM.on('refreshToken', (token) => {
-        if( !token ) { return console.warn("No firebase token on refresh."); }
-        api.sessions.updateFirebaseToken(token);
-      })
-
-      navigator.geolocation.getCurrentPosition(
-        (position) => {
-          var initialPosition = JSON.stringify(position);
-          api.pins.create({
-            lat: position.coords.latitude,
-            lng: position.coords.longitude,
-          }).then(function() {
-            return api.friends.nearby();
-          }).then((friends) => {
-            friends = friends.map(function(f) {
-              f.selected = true;
-              return f;
-            })
-            this.props.dispatch({type: 'nearbyFriends:load:yes', friends: friends});
-            this.setState({friends: friends, loading: false, allSelected: true});
-          }).catch(function(err) {
-            return console.error(err);
-          })
-        },
-        (error) => alert(JSON.stringify(error)),
-        {enableHighAccuracy: true, timeout: 20000, maximumAge: 1000}
-      );
-    }).catch((err) => {
-      this.props.dispatch({type: 'nearbyFriends:load:no', error: err});
-      this.setState({error: err, loading: false});
-    })
   }
 
   render() { return (
