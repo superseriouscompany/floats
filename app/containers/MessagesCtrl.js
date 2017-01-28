20dd635dab3dd0ee9cf5c5284802aa7fc4019cc8
Neil Sarkar <neil.r.sarkar@gmail.com>
2017-01-28 17:39:58 +0000

    Checkpoint: calculate state in MessagesCtrl

--- app/containers/MessagesCtrl.js
+++ app/containers/MessagesCtrl.js
@@ -11,7 +11,33 @@ class MessagesCtrl extends Component {
 }
 
 function mapStateToProps(state) {
-  return {};
+  const messages = state.messages[state.convos.activeConvoId];
+  const convo = state.convos.all && state.convos.all.find(function(c) {
+    return c.id == state.convos.activeConvoId;
+  });
+  const name = convo.users[0].id == state.user.id
+    ? convo.users[1].name : convo.users[0].name
+
+  return({
+    loading:  messages.loading,
+    error:    messages.error,
+    messages: (messages.all || []).map(decorate),
+    user:     state.user,
+    name:     name,
+  })
+}
+
+function decorate(message) {
+  return {
+    _id: message.id,
+    text: message.text,
+    createdAt: new Date(message.created_at),
+    user: {
+      _id: message.user.id,
+      name: message.user.name,
+      avatar: message.user.avatar_url,
+    }
+  }
 }
 
 export default connect(mapStateToProps)(MessagesCtrl);
