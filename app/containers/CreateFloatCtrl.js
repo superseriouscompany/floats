eefde0c4fc8272a43dd885729781d2eafa8b0e61
Neil Sarkar <neil.r.sarkar@gmail.com>
2017-01-28 17:08:16 +0000

    Checkpoint: add floats container

--- app/containers/CreateFloatCtrl.js
+++ app/containers/CreateFloatCtrl.js
@@ -4,18 +4,13 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import CreateFloatScene from '../components/CreateFloatScene';
 import { fetchNearbyFriends } from '../actions/nearbyFriends';
-import {
-  AsyncStorage
-} from 'react-native';
 
 class CreateFloatCtrl extends Component {
   constructor(props) {
     super(props);
-    this.state = {};
   }
 
   componentDidMount() {
-    this.setState({loading: true});
     this.props.dispatch(fetchNearbyFriends);
   }
 
