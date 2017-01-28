7a94964aef4f33c7236a064f82f5e5b4c82ef42c
Neil Sarkar <neil.r.sarkar@gmail.com>
2017-01-28 17:58:26 +0000

    Add fake message in to messages controller

--- app/containers/MessagesCtrl.js
+++ app/containers/MessagesCtrl.js
@@ -11,17 +11,40 @@ class MessagesCtrl extends Component {
 }
 
 function mapStateToProps(state) {
-  const messages = state.messages[state.convos.activeConvoId];
   const convo = state.convos.all && state.convos.all.find(function(c) {
     return c.id == state.convos.activeConvoId;
   });
   const name = convo.users[0].id == state.user.id
     ? convo.users[1].name : convo.users[0].name
 
+  const messages = state.messages[state.convos.activeConvoId];
+  const items = messages.all || [];
+
+  if( !items.length && isPrimary(state, convo) ) {
+    let float = state.myFloats.all.find((f) => {
+      return f.id === convo.float_id
+    })
+
+    float = float || state.invitations.all.find((f) => {
+      return f.id === convo.float_id
+    })
+
+    if( !float ) {
+      console.warn("Couldn't find float", convo.float_id);
+    } else {
+      items.push({
+        id: 'mock',
+        text: float.title,
+        user: float.user,
+        created_at: float.created_at,
+      })
+    }
+  }
+
   return({
     loading:  messages.loading,
     error:    messages.error,
-    messages: (messages.all || []).map(decorate),
+    messages: items.map(decorate),
     user:     state.user,
     name:     name,
   })
@@ -38,6 +61,15 @@ function decorate(message) {
       avatar: message.user.avatar_url,
     }
   }
+}
+
+function isPrimary(state, convo) {
+  if( convo.users.length > 2 ) { return true; }
+  const otherConvos = state.convos.all.filter((c) => {
+    return c.float_id === convo.float_id
+  })
+
+  return otherConvos.length == 1;
 }
 
 export default connect(mapStateToProps)(MessagesCtrl);
