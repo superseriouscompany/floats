d67ad080f8086fd7dbf5cd731f0bb2a319eee09e
Neil Sarkar <neil.r.sarkar@gmail.com>
2017-01-28 17:16:37 +0000

    Move inbox generation to FloatsCtrl

--- app/containers/FloatsCtrl.js
+++ app/containers/FloatsCtrl.js
@@ -1,5 +1,6 @@
 'use strict';
 
+import _ from 'lodash';
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import FloatsScene from '../components/FloatsScene';
@@ -22,8 +23,51 @@ class FloatsCtrl extends Component {
 }
 
 function mapStateToProps(state) {
+  const inbox = generateInbox(state.invitations, state.myFloats, state.convos);
+  const isEmpty = inbox && !inbox.length && state.invitations.all && !state.invitations.all.length;
+
   return {
+    invitations: state.invitations,
+    myFloats:    state.myFloats,
+    convos:      state.convos,
+    inbox:       inbox,
+    empty:       isEmpty,
   }
+}
+
+function generateInbox(invitations, myFloats, convos) {
+  if( !invitations.all || !myFloats.all || !convos.all ) { return null; }
+
+  let floats = {};
+
+  invitations.all.forEach(function(i) {
+    if( !i.attending ) { return; }
+    floats[i.id] = {
+      ...i,
+      time: i.created_at,
+    }
+  });
+
+  myFloats.all.forEach(function(f) {
+    floats[f.id] = {
+      ...f,
+      time: f.created_at,
+    }
+  })
+
+  convos.all.forEach(function(c) {
+    if( !floats[c.float_id] ) { return console.warn("Missing float for convo", c.id, c.float_id); }
+    floats[c.float_id].convos = floats[c.float_id].convos || [];
+    floats[c.float_id].convos.push(c);
+    if( c.message && c.message.created_at ) {
+      floats[c.float_id].time = Math.max(floats[c.float_id].time, c.message.created_at);
+    }
+  })
+
+  const inbox = _.values(floats).sort(function(a, b) {
+    return a.time < b.time;
+  })
+  return inbox;
 }
 
 export default connect(mapStateToProps)(FloatsCtrl);
