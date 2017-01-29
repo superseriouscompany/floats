4f1fbd2dda92f1d20dd87bde1446a58bbbc42267
Neil Sarkar <neil.r.sarkar@gmail.com>
2017-01-29 14:35:46 +0000

    Update convo preview

--- app/actions/messages.js
+++ app/actions/messages.js
@@ -4,7 +4,8 @@ import api from '../services/api'
 
 export function send(convo, message, pendingMessage) {
   return function(dispatch) {
-    dispatch({type: 'messages:append', message: pendingMessage, convoId: convo.id});
+    dispatch({type: 'messages:append', message:      pendingMessage, convoId: convo.id});
+    dispatch({type: 'convos:changePreview', message: pendingMessage, convoId: convo.id});
     api.messages.create(convo.float_id, convo.id, message.text).then(function(m) {
       dispatch({type: 'dirty'});
     }).catch(function(err) {
