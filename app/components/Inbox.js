94e97cf76769706b4f9cd24b09b0fcc84b667ea3
Santiago Garza <sgarza62@yahoo.com>
2017-01-30 01:05:34 +0000

    Change bottom padding on Inbox

--- app/components/Inbox.js
+++ app/components/Inbox.js
@@ -11,7 +11,7 @@ import {
 
 export default class Inbox extends Component {
   render() { return (
-    <View style={{paddingBottom: 80}}>
+    <View>
       { this.props.inbox.map((float, key) => (
         <Float key={key} float={float} />
       ))}
