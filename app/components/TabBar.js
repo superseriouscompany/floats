32156ef35bce339966462a92feaa8fdda1eaee89
Neil Sarkar <neil.r.sarkar@gmail.com>
2017-02-03 21:27:02 +0000

    Wire up logout and delete again

--- app/components/TabBar.js
+++ app/components/TabBar.js
@@ -4,9 +4,7 @@ import React, {Component} from 'react';
 import Text from './Text';
 import base from '../styles/base';
 import api from '../services/api';
-import { connectActionSheet } from '@exponent/react-native-action-sheet';
 import { connect } from 'react-redux';
-import { persistStore } from 'redux-persist'
 import {
   Alert,
   AsyncStorage,
@@ -16,7 +14,7 @@ import {
   View,
 } from 'react-native';
 
-class TabBar extends Component {
+export default class TabBar extends Component {
   render() { return (
     <View style={[styles.container]}>
       <TouchableOpacity style={styles.tabItem} onPress={() => this.props.navigator.navigate('FloatsScene')}>
@@ -33,7 +31,7 @@ class TabBar extends Component {
           <Image source={require('../images/Airplane.png')} />
         }
       </TouchableOpacity>
-      <TouchableOpacity style={styles.tabItem} onPress={() => this.showLogoutDialog()}>
+      <TouchableOpacity style={styles.tabItem} onPress={() => this.props.navigator.navigate('FriendsScene')}>
         { this.props.active == 'friends' ?
           <Image source={require('../images/ProfileActive.png')} />
         :
@@ -42,58 +40,7 @@ class TabBar extends Component {
       </TouchableOpacity>
     </View>
   )}
-
-  showLogoutDialog() {
-    //return this.props.navigator.navigate('FriendsScene')
-    this.props.showActionSheetWithOptions({
-      options: [`Logout`, `Delete Account`, 'Cancel'],
-      destructiveButtonIndex: 1,
-      cancelButtonIndex: 2,
-    }, (index) => {
-      if( index == 2 ) { return; }
-      if( index == 0 ) {
-        this.logout();
-      }
-      if( index == 1 ) {
-        Alert.alert(
-          'Delete Account',
-          'Are you sure?',
-          [
-            {text: 'Yes, delete me.', onPress: () => this.deleteAccount()},
-            {text: 'No', style: 'cancel'},
-          ]
-        )
-      }
-    })
-  }
-
-  deleteAccount() {
-    api.users.deleteAccount().then(() => {
-      this.logout();
-    }).catch(function(err) {
-      console.error(err);
-      alert("Sorry, something went wrong in deleting your account. Please try again or email support@superserious.co");
-    })
-  }
-
-  logout() {
-    AsyncStorage.removeItem('@floats:accessToken').then(() => {
-      return AsyncStorage.removeItem('@floats:user')
-    }).then(() => {
-      persistStore(this.context.store, {storage: AsyncStorage}).purge();
-      this.props.navigator.navigate('LoginScene');
-    }).catch(function(err) {
-      console.error(err);
-      alert("Sorry, we couldn't log you out. Please try again.")
-    })
-  }
 }
-
-TabBar.contextTypes = {
-  store: React.PropTypes.object
-}
-
-export default connectActionSheet(TabBar);
 
 const styles = StyleSheet.create({
   container: {
