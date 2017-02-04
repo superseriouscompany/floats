ba61f5a182ef071c0dc2d0e11fbad0e3ea1f2071
Santiago Garza <sgarza62@yahoo.com>
2017-02-04 13:57:39 +0000

    Add background image for background permissions and change statusbar color

--- app/components/BackgroundPermissionScene.js
+++ app/components/BackgroundPermissionScene.js
@@ -2,23 +2,35 @@ import React from 'react'
 import Text from './Text'
 import {
   Image,
+  StatusBar,
   StyleSheet,
   View,
 } from 'react-native'
 
 export default function(props) { return (
-  <View style={styles.container}>
-    <Text onPress={() => props.navigator.navigate('NotificationPermissionScene')}>
-      Background Permissions
-    </Text>
-  </View>
+  <Image style={styles.skyBackground} source={require('../images/SkyBackground.jpg')}>
+    <StatusBar
+       backgroundColor="blue"
+       barStyle="light-content"
+     />
+    <View style={styles.container}>
+      <Text onPress={() => props.navigator.navigate('NotificationPermissionScene')}>
+        Background Permissions
+      </Text>
+    </View>
+  </Image>
 )}
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: 'hotpink',
     justifyContent: 'center',
     alignItems: 'center',
-  }
+  },
+  skyBackground: {
+    flex: 1,
+    // remove width and height to override fixed static size
+    width: null,
+    height: null,
+  },
 })
