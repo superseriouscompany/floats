a3ee9674929ac5083b9403c66d3a5f00b5b25b7b
Neil Sarkar <neil.r.sarkar@gmail.com>
2017-02-05 14:55:46 +0000

    Checkpoint: grab other user id

--- app/containers/CreateFloatCtrl.js
+++ app/containers/CreateFloatCtrl.js
@@ -14,7 +14,8 @@ import {
 class CreateFloatCtrl extends Component {
   constructor(props) {
     super(props);
-    this.refresh = this.refresh.bind(this);
+    this.refresh          = this.refresh.bind(this);
+    this.invitationDialog = this.invitationDialog.bind(this)
   }
 
   componentDidMount() {
@@ -37,12 +38,15 @@ class CreateFloatCtrl extends Component {
   }
 
   invitationDialog() {
+    if( !this.props.user || !this.props.user.id ) {
+      console.warn("No user set", JSON.stringify(this.props));
+    }
+
     let branchUniversalObject = branch.createBranchUniversalObject(
-      'floats/join/cool',
+      `friends/invite/${this.props.user.id}`,
       {
         metadata: {
-          token: 'cool',
-          lawng: 'great',
+          inviter_id: this.props.user.id,
         }
       }
     )
@@ -54,15 +58,15 @@ class CreateFloatCtrl extends Component {
 
     let controlParams = {
       '$ios_deepview': 'floats_deepview_vk8d',
-      '$og_title': 'Cash me outside',
+      '$og_title': 'Cash me ousside',
       '$og_description': 'How bow dah',
+      '$og_image_url': 'https://pixel.nymag.com/imgs/daily/selectall/2017/02/02/02-catch-me-outside.w710.h473.2x.jpg',
     }
 
-    // 'https://floats.app.link/floats/join?token=cool'
     branchUniversalObject.generateShortUrl(linkProperties, controlParams).then((thing) => {
       ActionSheetIOS.showShareActionSheetWithOptions({
         url: thing.url,
-        message: 'Download this babbage',
+        message: 'Download Floats',
       }, (error) => {
         console.error(error);
         alert(error.message);
@@ -86,6 +90,7 @@ class CreateFloatCtrl extends Component {
 
 function mapStateToProps(state) {
   return {
+    user:      state.user,
     loading:   state.nearbyFriends.loading,
     error:     state.nearbyFriends.error,
     friends:   state.nearbyFriends.items,
--- app/containers/DeepLinkCtrl.js
+++ app/containers/DeepLinkCtrl.js
@@ -15,6 +15,7 @@ class DeepLinkCtrl extends Component {
   constructor(props) {
     super(props);
     this.handleLink = this.handleLink.bind(this)
+    this.addFriend  = this.addFriend.bind(this)
   }
 
   componentWillMount() {
@@ -27,13 +28,23 @@ class DeepLinkCtrl extends Component {
     Linking.addEventListener('url', this.handleLink);
 
     branch.subscribe((bundle) => {
+      if( !bundle ) { return console.warn(`Got empty deep link`); }
       console.warn(`Got deep link ${JSON.stringify(bundle)}`);
-      if (bundle && bundle.params && !bundle.error) {}
+      if( bundle.params && !bundle.error ) {
+        if( bundle.params['~feature'] == 'friend-invitation' ) {
+          return this.addFriend(bundle.params.inviter_id);
+        }
+      }
     })
   }
 
   componentWillUnmount() {
-    Linking.removeEventListener('url', this.handleLink)
+    Linking.removeEventListener('url', this.handleLink);
+  }
+
+  addFriend(id) {
+    if( !id ) { return console.warn('No id set in addFriend'); }
+    alert('Adding ' + id);
   }
 
   handleLink(event) {
