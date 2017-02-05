0fe86c7a863c8096278e4f7d1b5b2284074c6c2b
Santiago Garza <sgarza62@yahoo.com>
2017-02-05 16:58:04 +0000

    Fix Permissions Pages so they look like they have a button

--- app/components/NotificationPermissionScene.js
+++ app/components/NotificationPermissionScene.js
@@ -34,12 +34,16 @@ export default function(props) { return (
       <Text style={styles.subText}>
         you can leave a float at any time
       </Text>
-      <TouchableOpacity style={styles.button} onPress={() => props.navigator.navigate('CreateFloatScene')}>
-        <Text style={styles.mainText}>
-          Allow Notifications
-        </Text>
-      </TouchableOpacity>
     </View>
+
+    <TouchableOpacity style={styles.button} onPress={() => props.navigator.navigate('CreateFloatScene')}>
+      <Text style={[styles.mainText, {}]}>
+        Allow Notifications
+      </Text>
+      <View style={styles.rightArrow}>
+        <Image source={require('../images/RightArrowLight.png')}/>
+      </View>
+    </TouchableOpacity>
   </Image>
 )}
 
@@ -129,9 +133,9 @@ const styles = StyleSheet.create({
     backgroundColor: 'white',
     justifyContent: 'center',
     alignItems: 'center',
-    paddingLeft: 30,
-    paddingRight: 30,
-    paddingBottom: 64,
+    paddingLeft: 25,
+    paddingRight: 25,
+    marginBottom: 68,
   },
   mainText: {
     textAlign: 'center',
@@ -144,13 +148,21 @@ const styles = StyleSheet.create({
   },
   button: {
     position: 'absolute',
+    flexDirection: 'row',
+    backgroundColor: 'white',
     left: 0,
     right: 0,
     justifyContent: 'center',
     alignItems: 'center',
     bottom: 0,
     height: 65,
-    borderWidth: 1,
-    borderColor: base.colors.lightgrey,
   },
+  rightArrow: {
+    position: 'absolute',
+    right: 0,
+    top: 0,
+    bottom: 0,
+    paddingRight: 15,
+    justifyContent: 'center'
+  }
 })
