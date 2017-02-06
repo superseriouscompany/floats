470a199ba3e1984a87b292566b64e68a742752e5
Neil Sarkar <neil.r.sarkar@gmail.com>
2017-02-06 03:09:18 +0000

    Checkpoint: add react-native-permissions and AuthedCtrl

--- app/containers/Root.js
+++ app/containers/Root.js
@@ -1,19 +1,12 @@
 import React from 'react';
 import { Provider } from 'react-redux';
 import { ActionSheetProvider } from '@exponent/react-native-action-sheet';
-import CreateFloatCtrl from '../containers/CreateFloatCtrl';
-import FloatsCtrl from '../containers/FloatsCtrl';
-import MessagesCtrl from '../containers/MessagesCtrl';
 import KillSwitchCtrl from '../containers/KillSwitchCtrl';
-import FriendsCtrl from '../containers/FriendsCtrl';
 import PushCtrl from '../containers/PushCtrl';
 import DeepLinkCtrl from '../containers/DeepLinkCtrl';
 import LoginScene from '../components/LoginScene';
-import RandosScene from '../components/RandosScene';
-import BackgroundPermissionScene from '../components/BackgroundPermissionScene';
-import NotificationPermissionScene from '../components/NotificationPermissionScene';
+import AuthedCtrl from '../containers/AuthedCtrl';
 import Scratch from '../components/Scratch';
-import Text from '../components/Text';
 import api from '../services/api';
 import store from '../services/store';
 import God from '../services/god';
@@ -69,26 +62,10 @@ export default class Root extends Component {
                 <View style={{flex: 1}}>
                   { this.state.scene == 'LoginScene' ?
                     <LoginScene navigator={this.navigator} />
-                  : this.state.scene == 'CreateFloatScene' ?
-                    <CreateFloatCtrl navigator={this.navigator} />
-                  : this.state.scene == 'FloatsScene' ?
-                    <FloatsCtrl navigator={this.navigator} />
-                  : this.state.scene == 'FriendsScene' ?
-                    <FriendsCtrl navigator={this.navigator} />
-                  : this.state.scene == 'RandosScene' ?
-                    <RandosScene navigator={this.navigator} />
-                  : this.state.scene == 'MessagesScene' ?
-                    <MessagesCtrl navigator={this.navigator} />
-                  : this.state.scene == 'BackgroundPermissionScene' ?
-                    <BackgroundPermissionScene navigator={this.navigator} />
-                  : this.state.scene == 'NotificationPermissionScene' ?
-                    <NotificationPermissionScene navigator={this.navigator} />
                   : this.state.scene == 'Scratch' ?
-                    <Scratch />
-                  : !!this.state.scene ?
-                    <Text style={{padding: 200}}>404</Text>
+                    <Scratch navigator={this.navigator} />
                   :
-                    null
+                    <AuthedCtrl scene={this.state.scene} navigator={this.navigator} />
                   }
                 </View>
               </PushCtrl>
